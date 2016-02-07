1d47c6e4c2be3b691efe313077d669b06fa5324c
Manuel Amador (Rudd-O) <rudd-o@rudd-o.com>
2016-02-07 03:52:35 +0000

    add the ability for the parser to parse the contents of transactions, wrongly, but better than nothing

--- src/ledgerhelpers/parser.py
+++ src/ledgerhelpers/parser.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 
+import collections
 import ledgerhelpers
 from ledgerhelpers import diffing
 
@@ -7,7 +8,14 @@ from ledgerhelpers import diffing
 CHAR_ENTER = "\n"
 CHAR_COMMENT = ";#"
 CHAR_NUMBER = "1234567890"
+CHAR_TAB = "\t"
 CHAR_WHITESPACE = " \t"
+CHAR_CLEARED = "*"
+CHAR_PENDING = "!"
+
+STATE_CLEARED = CHAR_CLEARED
+STATE_PENDING = CHAR_PENDING
+STATE_UNCLEARED = None
 
 
 def pos_within_items_to_row_and_col(pos, items):
@@ -25,11 +33,13 @@ def pos_within_items_to_row_and_col(pos, items):
 
 
 def parse_date_from_transaction_contents(contents):
-    splits = CHAR_WHITESPACE + "="
-    data = contents
-    for s in splits:
-        data = data.split(s)[0]
-    return ledgerhelpers.parse_date(data)
+    return ledgerhelpers.parse_date(u"".join(contents))
+
+
+TransactionPosting = collections.namedtuple(
+    'TransactionPosting',
+    ['account', 'amount']
+)
 
 
 class Token(object):
@@ -49,6 +59,14 @@ class TokenComment(Token):
     pass
 
 
+class TokenTransactionClearedFlag(Token):
+    pass
+
+
+class TokenTransactionPendingFlag(Token):
+    pass
+
+
 class TokenWhitespace(Token):
     pass
 
@@ -57,13 +75,75 @@ class TokenTransaction(Token):
 
     def __init__(self, pos, contents):
         Token.__init__(self, pos, contents)
-        self.date = parse_date_from_transaction_contents(self.contents)
+        lexer = LedgerTransactionLexer(contents)
+        lexer.run()
+
+        def find_token(klass):
+            try:
+                return [t for t in lexer.tokens if isinstance(t, klass)][0]
+            except IndexError:
+                return None
+
+        try:
+            self.date = find_token(TokenTransactionDate).date
+        except AttributeError:
+            raise TransactionLexingError("no transaction date in transaction")
+
+        try:
+            self.secondary_date = find_token(
+                TokenTransactionSecondaryDate
+            ).date
+        except AttributeError:
+            self.secondary_date = None
+
+        if find_token(TokenTransactionClearedFlag):
+            self.state = STATE_CLEARED
+        elif find_token(TokenTransactionPendingFlag):
+            self.state = STATE_PENDING
+        else:
+            self.state = STATE_UNCLEARED
+
+        if self.state != STATE_UNCLEARED:
+            self.clearing_date = (
+                self.secondary_date if self.secondary_date else self.date
+            )
+        else:
+            self.clearing_date = None
+
+        try:
+            self.payee = find_token(TokenTransactionPayee).payee
+        except AttributeError:
+            raise TransactionLexingError("no payee in transaction")
+
+        accountsamounts = [
+            t for t in lexer.tokens
+            if isinstance(t, TokenTransactionPostingAccount) or
+            isinstance(t, TokenTransactionPostingAmount)
+        ]
+
+        x = []
+        last = None
+        for v in accountsamounts:
+            if isinstance(v, TokenTransactionPostingAccount):
+                assert type(last) in [
+                     type(None), TokenTransactionPostingAmount
+                ], lexer.tokens
+            elif isinstance(v, TokenTransactionPostingAmount):
+                assert type(last) in [
+                    TokenTransactionPostingAccount
+                ], lexer.tokens
+                x.append(TransactionPosting(last.account, v.amount))
+            last = v
+        assert len(x) * 2 == len(accountsamounts), lexer.tokens
+        self.postings = x
 
 
 class TokenTransactionWithContext(TokenTransaction):
 
     def __init__(self, pos, tokens):
-        self.transaction = [ t for t in tokens if isinstance(t, TokenTransaction) ][0]
+        self.transaction = [
+            t for t in tokens if isinstance(t, TokenTransaction)
+        ][0]
         self.pos = pos
         self.contents = u"".join(t.contents for t in tokens)
 
@@ -84,11 +164,50 @@ class TokenEmbeddedPython(Token):
     pass
 
 
+class TokenTransactionPostingAccount(Token):
+
+    def __init__(self, pos, contents):
+        Token.__init__(self, pos, contents)
+        self.account = u''.join(contents)
+
+
+class TokenTransactionPostingAmount(Token):
+
+    def __init__(self, pos, contents):
+        Token.__init__(self, pos, contents)
+        self.amount = u''.join(contents)
+
+
 class TokenEmbeddedTag(Token):
     pass
 
 
+class TokenTransactionDate(Token):
+
+    def __init__(self, pos, contents):
+        Token.__init__(self, pos, contents)
+        self.date = parse_date_from_transaction_contents(self.contents)
+
+
+class TokenTransactionSecondaryDate(Token):
+
+    def __init__(self, pos, contents):
+        Token.__init__(self, pos, contents)
+        self.date = parse_date_from_transaction_contents(self.contents)
+
+
+class TokenTransactionPayee(Token):
+
+    def __init__(self, pos, contents):
+        Token.__init__(self, pos, contents)
+        self.payee = u''.join(contents)
+
+
 class LexingError(Exception):
+    pass
+
+
+class TransactionLexingError(Exception):
     pass
 
 
@@ -140,9 +259,6 @@ class GenericLexer(object):
         """Creates an instance of klass (a Token class) with the current
         position and the supplied items as parameters, then
         accumulates the instance into the self.tokens accumulator."""
-        if len(items) == 0:
-            self._last_emitted_pos = self.pos
-            return
         token = klass(self.pos, items)
         self._last_emitted_pos = self.pos
         self.tokens += [token]
@@ -258,6 +374,117 @@ class LedgerTextLexer(GenericLexer):
                         l, c, l2, c2, e
                     )
                 )
+
+
+class LedgerTransactionLexer(GenericLexer):
+
+    def __init__(self, text):
+        GenericLexer.__init__(self, text)
+
+    def state_parsing_transaction_date(self):
+        chars = []
+        while self.more():
+            if self.peek() not in "0123456789-/":
+                self.emit(TokenTransactionDate, chars)
+                if self.confirm_next("="):
+                    self.next()
+                    return self.state_parsing_clearing_date
+                elif self.peek() in CHAR_WHITESPACE:
+                    return self.state_parsing_cleared_flag_or_payee
+                else:
+                    raise TransactionLexingError("invalid character %s" % self.peek())
+            chars += [self.next()]
+        raise TransactionLexingError("incomplete transaction")
+
+    def state_parsing_clearing_date(self):
+        chars = []
+        while self.more():
+            if self.peek() not in "0123456789-/":
+                self.next()
+                self.emit(TokenTransactionSecondaryDate, chars)
+                return self.state_parsing_cleared_flag_or_payee
+            chars += [self.next()]
+        raise TransactionLexingError("incomplete transaction")
+
+    def state_parsing_cleared_flag_or_payee(self):
+        while self.more():
+            if self.peek() in CHAR_WHITESPACE:
+                self.next()
+                continue
+            if self.peek() in CHAR_ENTER:
+                break
+            if self.confirm_next(CHAR_CLEARED):
+                self.emit(TokenTransactionClearedFlag, [self.next()])
+                return self.state_parsing_payee
+            if self.confirm_next(CHAR_PENDING):
+                self.emit(TokenTransactionPendingFlag, [self.next()])
+                return self.state_parsing_payee
+            return self.state_parsing_payee
+        raise TransactionLexingError("incomplete transaction")
+
+    def state_parsing_payee(self):
+        return self.state_parsing_rest_of_line(
+            TokenTransactionPayee,
+            self.state_parsing_transaction_posting_indentation)
+
+    def state_parsing_rest_of_line(
+        self,
+        klass, next_state,
+        allow_empty_values=False
+    ):
+        chars = []
+        while self.more():
+            if self.peek() in CHAR_ENTER:
+                self.next()
+                while chars and chars[-1] in CHAR_WHITESPACE:
+                    chars = chars[:-1]
+                break
+            if self.peek() in CHAR_WHITESPACE and not chars:
+                self.next()
+                continue
+            chars.append(self.next())
+        if allow_empty_values or chars:
+            self.emit(klass, chars)
+            return next_state
+        raise TransactionLexingError("incomplete transaction")
+
+    def state_parsing_transaction_posting_indentation(self):
+        chars = []
+        while self.more():
+            if self.peek() not in CHAR_WHITESPACE:
+                break
+            chars.append(self.next())
+        if not chars:
+            return
+        return self.state_parsing_transaction_posting_account
+
+    def state_parsing_transaction_posting_account(self):
+        chars = []
+        while self.more():
+            if (
+                (self.peek() in CHAR_WHITESPACE and
+                 chars and chars[-1] in CHAR_WHITESPACE) or
+                self.peek() in CHAR_TAB
+            ):
+                while chars[-1] in CHAR_WHITESPACE:
+                    chars = chars[:-1]
+                break
+            chars.append(self.next())
+        if not chars:
+            raise TransactionLexingError("truncated transaction posting")
+        self.emit(TokenTransactionPostingAccount, chars)
+        return self.state_parsing_transaction_posting_amount
+
+    def state_parsing_transaction_posting_amount(self):
+        return self.state_parsing_rest_of_line(
+            TokenTransactionPostingAmount,
+            self.state_parsing_transaction_posting_indentation,
+            allow_empty_values=True)
+
+    def run(self):
+        state = self.state_parsing_transaction_date
+        while state:
+            state = state()
 
 
 class LedgerContextualLexer(GenericLexer):
