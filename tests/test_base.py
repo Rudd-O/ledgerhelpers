3945323caed210d6e1943381334d8cdc978b084e
Marcin Owsiany <porridge@debian.org>
2022-11-08 21:52:12 +0100

    Optionally enable debugging in tests.

--- tests/test_base.py
+++ tests/test_base.py
@@ -1,6 +1,12 @@
 import codecs
 import os
 
+import ledgerhelpers as m
+
+
+if os.getenv('LEDGERHELPERS_TEST_DEBUG'):
+    m.enable_debugging(True)
+
 
 def datapath(filename):
     return os.path.join(os.path.dirname(__file__), "testdata", filename)
